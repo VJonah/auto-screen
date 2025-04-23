a27e9f7445d9fd9f1ccba3803ebddbad862e7966
jonah <vaironj@pm.me>
2025-04-23 10:36:03 +0100

    module fix for optimisation

--- modules.py
+++ modules.py
@@ -6,16 +6,14 @@ import dspy
 from signatures import InclusionExclusionCriteria, CheckCriteria
 
 class ClassifyByInclusionExclusion(dspy.Module):
-    def __init__(self, sr_title: str):
-        self.sr_title = sr_title
+    def __init__(self):
         self.generate_criteria = dspy.ChainOfThought(InclusionExclusionCriteria)
         self.evaluate_criteria = dspy.ChainOfThought(CheckCriteria)
 
-    def forward(self, citation_title: str, citation_abstract: str):
+    def forward(self, sr_title: str, citation_title: str, citation_abstract: str):
         criteria = self.generate_criteria(
-            systematic_review_title=self.sr_title
+            systematic_review_title=sr_title
         ).criteria
         return self.evaluate_criteria(criteria=criteria,
                                       citation_title=citation_title,
                                       citation_abstract=citation_abstract)
-                                      
